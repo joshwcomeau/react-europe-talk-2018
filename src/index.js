ffd8fc57b2d037ed9a3d243bf6a8e30c8ae1bbdd
Joshua Comeau <joshwcomeau@gmail.com>
2018-04-01 07:53:50 -0400

    Hack-fix Spectacle black text

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { injectGlobal } from 'styled-components';
 
 import Presentation from './presentation';
 import registerServiceWorker from './registerServiceWorker';
 
 import './assets/prism.js';
 import './assets/prism.css';
+
+injectGlobal`
+  .spectacle-content > pre {
+    /* HACK: For some reason, spectacle-code-slide decided to start adding
+    black-coloured text. I'm in a rush, so I don't have time to figure out
+    why that is. Overriding it here sincei  don */
+    color: #FFF !important;
+  }
+`;
 
 ReactDOM.render(<Presentation />, document.getElementById('root'));
 registerServiceWorker();
